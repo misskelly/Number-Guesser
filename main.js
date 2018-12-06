65ddaf6ee7efc231003ee9c9b0e2106a52dd23c3
Joshua Lavarine <joshua@lavarine.com>
2018-12-06 09:45:59 -0700

    Create javascript variables

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 // Global variables at the top
 
 // names for challenger one 
-var challengerOne 
+var challengerOne = document.querySelector(".name");
 
 // name for challenger two 
+var challengerTwo = document.querySelector(".name-input-two");
+
 // guess one number
+var guessValueOne = document.querySelector(".guess-input-one");
+
 // guess two number
+var guessValueTwo = document.querySelector(".guess-input-two");
+
 // min-range
+var minRange = document.querySelector(".min-range");
+
 // max-range
-// number of guesses
+var maxRange = document.querySelector(".max-range");
+
+// number of guesses-!!Anonymous Function!!
+var totalGuesses = 
 
 
 
