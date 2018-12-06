c87559eeec247a1286c2a2e8c877052f8ae87fe8
Joshua Lavarine <joshua@lavarine.com>
2018-12-05 17:16:36 -0700

    Create main.js and add pseudo code

---
+++ main.js
@@ -0,0 +1,51 @@
+// Global variables at the top
+
+// names for challenger one 
+var challengerOne 
+
+// name for challenger two 
+// guess one number
+// guess two number
+// min-range
+// max-range
+// number of guesses
+
+
+
+
+
+
+
+
+
+// Functions
+
+// event listeners usually call functions
+
+
+
+
+
+
+
+
+
+
+// eventListeners
+
+// update range (button update)
+// submit guess (button guess)
+// reset game (button reset)
+// clear game (button clear)
+
+
+
+
+
+
+
+
+
+
+
+
